e316d045643e65d5e29f3c6e7329d103646e7895
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-03-06 11:39:06 +0100

    Override CalculateStatistics in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,5 +22,14 @@ namespace GradeBook.GradeBooks {
 			if (percentGradePosition <= 80) return'D';
 			return'F';
 		}
+
+		public override void CalculateStatistics() {
+			if (Students.Count() < 5) {
+				Console.WriteLine("Ranked grading requires at least 5 students.");
+			}
+			else {
+				base.CalculateStatistics();
+			}
+		}
 	}
 }
